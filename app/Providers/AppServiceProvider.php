48d6c71cc3dfb4b359d7340598808dd5f91b5381
dimsard <d.ardiansyah999@gmail.com>
2025-07-15 13:20:46 +0700

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,7 +22,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
+        if (config('database.default') === 'sqlite') {
+            $dbPath = database_path('database.sqlite');
+            if (!File::exists($dbPath)) {
+                File::put($dbPath, '');
+            }
+        }
     }
 }
