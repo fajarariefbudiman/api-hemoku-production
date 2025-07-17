e4368b81316091ea8451d9d1fcd1da67186e25b1
dimsard <d.ardiansyah999@gmail.com>
2025-07-17 20:02:17 +0700

    first commit

--- database/migrations/2025_07_07_002953_create_educational_contents_table.php
+++ database/migrations/2025_07_07_002953_create_educational_contents_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('educational_contents', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->unique();
             $table->longText('content')->nullable();
             $table->string('section')->nullable();
             $table->text('description')->nullable();
