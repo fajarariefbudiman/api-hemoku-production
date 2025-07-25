715a1d810857818858059d9f88d912d53875bf9f
dimsard <d.ardiansyah999@gmail.com>
2025-07-25 21:34:06 +0700

    add poster-link-test

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Response;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
-
-Route::get('/storage/posters/{filename}', function ($filename) {
-    $path = storage_path('storage/app/public/posters/' . urldecode($filename));
-
-    if (!file_exists($path)) {
-        abort(404);
-    }
-
-    return Response::file($path);
-})->where('filename', '.*');
