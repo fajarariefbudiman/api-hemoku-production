89e6fa7b16b149e53ee282889f3f3e2921b7b227
dimsard <d.ardiansyah999@gmail.com>
2025-07-30 15:32:10 +0700

    first commit prod

--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-$_SERVER['SCRIPT_NAME'] = str_replace('/hemoku', '', $_SERVER['SCRIPT_NAME']);
-$_SERVER['PHP_SELF'] = str_replace('/hemoku', '', $_SERVER['PHP_SELF']);
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
